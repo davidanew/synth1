04984a8e6c3de777fe98b6a6ef28a994b600b079
David New <46327493+davidanew@users.noreply.github.com>
2019-07-17 11:10:45 +0100

    Responding to keybourd key press

--- main.cpp
+++ main.cpp
@@ -20,25 +20,26 @@ void SystemClock_Config(void);
 
 
 
-/*
-UART_HandleTypeDef huart2;
+
+UART_HandleTypeDef huart1;
 static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 
 int main () {
   //testing code
 	Hal::init();
 	MX_GPIO_Init();
-  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
 	uint8_t buffer[4] = {1,2,3,4};
-  //HAL_UART_Receive(&huart2, buffer, sizeof(buffer), HAL_MAX_DELAY);
 	while (1)
-		HAL_UART_Transmit(&huart2, buffer, sizeof(buffer), HAL_MAX_DELAY );
-	while(1);
+  HAL_UART_Receive(&huart1, buffer, sizeof(buffer), HAL_MAX_DELAY);
+	//while (1)
+	//	HAL_UART_Transmit(&huart1, buffer, sizeof(buffer), HAL_MAX_DELAY );
+	//while(1);
 }
-*/
 
 
+/*
 
 static UART_HandleTypeDef s_UARTHandle = UART_HandleTypeDef();
  
@@ -98,7 +99,9 @@ void ms_delay (int ms)
 	__asm ("nop");
     }
 }
-		
+
+*/
+
 /*
 
 int main (void)
@@ -243,21 +246,21 @@ void SystemClock_Config(void)
   }
 }
 
-/*
 
-static void MX_USART2_UART_Init(void)
+
+static void MX_USART1_UART_Init(void)
 {
 
 
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 31250;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 31250;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
   {
     while(1);
   }
@@ -268,36 +271,46 @@ static void MX_USART2_UART_Init(void)
 
 static void MX_GPIO_Init(void)
 {
-
-
   __HAL_RCC_GPIOA_CLK_ENABLE();
-
 }
 
 
 void HAL_UART_MspInit(UART_HandleTypeDef* huart)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(huart->Instance==USART2)
+  if(huart->Instance==USART1)
   {
 
-    __HAL_RCC_USART2_CLK_ENABLE();
+    __HAL_RCC_USART1_CLK_ENABLE();
   
     __HAL_RCC_GPIOA_CLK_ENABLE();
+		
+		/*
 
-    //PA2     ------> USART2_TX
-    //PA3     ------> USART2_RX 
-
-    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+		*/
+		
+
+ 
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   }
 
 }
 
-*/
+
 
